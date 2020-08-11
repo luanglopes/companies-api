01de7d62b98736ed292227035ea7082ae57af0b1
Luan Lopes <luanglopes@gmail.com>
2020-08-10 23:53:01 -0300

    feat: add async errors and json support to express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import 'express-async-errors'
 
 const app = express()
 
 app.use(cors())
 app.use(helmet())
+app.use(express.json())
 
 app.get('/health', (request, response) => {
     return response.json({ status: 'Running' })
