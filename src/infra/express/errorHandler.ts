cce97f49aaacfefd25ddcb33dc98b5241ff08b18
Luan Lopes <luanglopes@gmail.com>
2020-08-14 01:31:59 -0300

    refactor: improve error handling

--- src/infra/express/errorHandler.ts
+++ src/infra/express/errorHandler.ts
@@ -25,11 +25,12 @@ const compileCelebrateErrors = (details: CelebrateInternalError['joi']['details'
   return Object.values(fieldsObj)
 }
 
-export const errorHandler: ErrorRequestHandler = (error: Error | CelebrateInternalError, request: Request, response: Response, _: NextFunction) => {
+export const errorHandler: ErrorRequestHandler = (error: Error | CelebrateInternalError, request: Request, response: Response, _next: NextFunction) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: 'error',
       error: 'AppError',
+      code: error.code,
       message: error.message,
       details: error.details,
     })
@@ -42,8 +43,8 @@ export const errorHandler: ErrorRequestHandler = (error: Error | CelebrateIntern
     return response.status(400).json({
       status: 'error',
       error: 'ValidationError',
-      message:
-        'Some fields have errors, make sure you are providing valid values',
+      code: 'VALIDATION_ERROR',
+      message: 'Some fields have errors, make sure you are providing valid values',
       fields: errors,
     })
   }
@@ -53,6 +54,7 @@ export const errorHandler: ErrorRequestHandler = (error: Error | CelebrateIntern
   return response.status(500).json({
     status: 'error',
     error: 'UnknownError',
+    code: 'UNKNOWN_ERROR',
     message: 'Internal Server Error',
   })
 }
